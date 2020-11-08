f2f37eff0b308e5254fc911adbce64204fc82054
timwu2016 <gw923@nyu.edu>
2020-11-08 13:50:44 +0800

    Update and add new functions

--- useful_functions.py
+++ useful_functions.py
@@ -34,24 +34,46 @@ def import_libraries():
   global_import('re')
   pd.set_option('display.max_columns', 100)
   pd.set_option('display.max_rows', 100)
+  global default_dpi
+  default_dpi = 90
   print('Importing Done')
 
-
-import_libraries()
 
 # -------------------------------------Data Selection----------------------------------------------
 
 
+def load_data(which_year):
+  '''Provide the census year you will to load (choose from 1850, 1880, 1910)'''
+  try:
+    df = pd.read_csv('/content/drive/My Drive/census_' + str(which_year) + '.csv', low_memory=False)
+    return df
+  except FileNotFoundError as e:
+    print('File Not Found! Please check if you have created Shortcuts for the data files\nin your "My Drive" folder and if you have run the first cell in this notebook.')
+  except NameError as e:
+    print('Function not defined yet! Please check if you have run the first cell in this notebook.')
+
+
 def build_criteria_from_string(string, data):
-  string = string.replace(' is ', ' = ')
-  col = string.split('=')[0].strip()
-  value = string.split('=')[1].strip()
-  value = int(value) if value.isnumeric() else value
-  return build_criteria(col, value, data)
+
+  if ' is not ' in string or ' != ' in string:
+    string = string.replace(' is not ', ' != ')
+    col = string.split('!=')[0].strip()
+    value = string.split('!=')[1].strip()
+    value = float(value) if value.isnumeric() else value
+    return build_criteria(col, value, data, equal_or_not=False)
+  elif ' is ' in string or ' = ' in string:
+    string = string.replace(' is ', ' = ')
+    col = string.split('=')[0].strip()
+    value = string.split('=')[1].strip()
+    value = float(value) if value.isnumeric() else value
+    return build_criteria(col, value, data, equal_or_not=True)
 
 
-def build_criteria(col, value, data):
-  return data[col] == value
+def build_criteria(col, value, data, equal_or_not=True):
+  if equal_or_not:
+    return data[col] == value
+  else:
+    return data[col] != value
 
 
 def get_multiple_criteria(string, data):
@@ -65,5 +87,88 @@ def get_multiple_criteria(string, data):
 
 
 def select_data(criteria_string, data):
+  '''Provide a comma separated criteria_string, and specify which dataframe (df) to select from'''
   criteria_filter = get_multiple_criteria(criteria_string, data)
   return data[criteria_filter].copy()
+
+# -------------------------------------Smart Data Description-------------------------------------------
+
+def describe(col, data=df, top_k=-1, thres=90, return_full=False, plot_top_k=-1, plot_type='', bins=-1):
+
+  if data[col].isnull().mean() > 0:
+    print(f"Notice: {np.round(data[col].isnull().mean()*100,3)}% of the entries have no records for this field.\n")
+
+  data_numeric_columns = data.dtypes[data.dtypes.apply(lambda x: np.issubdtype(x, np.number))].index.tolist()
+
+  if col in data_numeric_columns:
+    if bins == -1:
+      print(f'Change the default width of histogram bars by setting "bins = <a number>".\n')
+      bins = 50
+    plt.figure(figsize=(9, 6), dpi=default_dpi)
+    plt.hist(data[col].dropna(), bins=bins)
+    plt.title(f"Distribution of the {col}")
+    return
+
+  ser = data[col].value_counts()
+  ser.name = 'Absolute Number'
+
+  percentage_ser = np.round(ser / len(data) * 100, 2)
+  percentage_ser.name = 'Proportion in Data (%)'
+
+  cumsum_percentage_ser = percentage_ser.cumsum()
+  cumsum_percentage_ser.name = 'Cumulative Proportion (%)'
+
+  full_value_counts_df = pd.concat([ser, percentage_ser, cumsum_percentage_ser], axis=1)
+
+  if plot_top_k > top_k:
+    top_k = plot_top_k
+
+  if top_k == -1:
+    top_k = sum(cumsum_percentage_ser <= thres) + 1
+    top_k = max(5, top_k)
+    top_k = min(20, top_k)
+
+  value_counts_df = full_value_counts_df if return_full else full_value_counts_df[:top_k]
+
+  if top_k < len(full_value_counts_df) and not return_full:
+    print(f'{len(full_value_counts_df)-top_k} more rows are available, add "return_full = True" if you want to see all.\n')
+
+  plot_top_k = 10 if plot_top_k == -1 else plot_top_k
+  graph_df = value_counts_df['Proportion in Data (%)'][:plot_top_k].copy()
+
+  if plot_type == '':
+    plot_type = 'bar' if graph_df.sum() < thres else 'pie'
+
+  if plot_type == 'pie':
+
+    fig, ax = plt.subplots(figsize=(9, 6), dpi=default_dpi, subplot_kw=dict(aspect="equal"))
+
+    values = graph_df.values.tolist()
+    names = graph_df.index.tolist()
+
+    def func(pct, allvals):
+      absolute = int(pct / 100. * np.sum(allvals))
+      return "{:.1f}%".format(pct, absolute)
+
+    wedges, texts, autotexts = ax.pie(values, autopct=lambda pct: func(pct, values), textprops=dict(color="w"))
+
+    for w in wedges:
+      w.set_edgecolor('white')
+
+    ax.legend(wedges, names,
+              title="Categories",
+              loc="center left",
+              bbox_to_anchor=(1, 0, 0.8, 1))
+
+    plt.setp(autotexts, size=12, weight="bold")
+
+    ax.set_title(f"Relative Proportion of Top {len(graph_df)} {col}" if len(graph_df) < len(full_value_counts_df) else f"Proportion of {col}")
+
+  if plot_type == 'bar':
+    plt.figure(figsize=(9, 6), dpi=default_dpi)
+    graph_df.plot(kind='bar')
+    plt.title(f"Barplot of the Top {len(graph_df)} {col} - (y axis shows percentage)")
+
+  print()
+
+  return value_counts_df
