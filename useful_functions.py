5abb515f6204d484e86c11d7cb01cb18119d6211
timwu2016 <gw923@nyu.edu>
2020-11-08 16:55:59 +0800

    Test behavior of select_data 2

--- useful_functions.py
+++ useful_functions.py
@@ -151,7 +151,8 @@ def get_multiple_criteria(string, data):
 
 def select_data(criteria_string, data):
   '''Provide a comma separated criteria_string, and specify which dataframe (df) to select from'''
-  criteria_filter = get_multiple_criteria(criteria_string, data.copy())
+  data = data.reset_index(drop=True).copy()
+  criteria_filter = get_multiple_criteria(criteria_string, data)
   return data[criteria_filter].copy()
 
 # -------------------------------------Smart Data Description-------------------------------------------
